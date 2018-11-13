94ebc8daa9499f1dd60e22a976b3172b5accb626
cesare <laurenziello@hotmail.it>
2018-11-13 18:08:10 +0100

    Add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   fillerNav = Array.from({ length: 10 }, (_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array.from({ length: 10 }, () =>
+  fillerContent = Array.from({ length: 50 }, () =>
     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
